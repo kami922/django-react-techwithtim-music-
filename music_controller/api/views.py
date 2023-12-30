0dd2c31d545dcd0303538b71aa78190dc3bb724b
kami922 <kamranulhaq2002@gmail.com>
2023-12-30 19:58:26 +0500

    testb 3rd commit

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(requ):
     return HttpResponse("Hello")
+
+def hello(requ):
+    return HttpResponse("sec function for git branches")
